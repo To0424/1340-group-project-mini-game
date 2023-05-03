c3a17cca33fbbd778ae94fdd8346e44f97a1dafb
To0424 <127820039+To0424@users.noreply.github.com>
2023-05-03 22:32:59 +0800

    Update shop.cpp

--- shop.cpp
+++ shop.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <windows.h>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
-int main() {
+string showshop(int &gold, int &ATK, int &HP){
     system("cls");
-
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); 
-    int gold = 10;
     SetConsoleTextAttribute(hConsole, 11);
     cout <<endl;
     cout <<"                      .oooooo..o ooooo   ooooo   .oooooo.   ooooooooo.                         "<<endl;
@@ -23,6 +22,8 @@ int main() {
     cout << "============================================================================================" <<endl;
     SetConsoleTextAttribute(hConsole, 6); 
     cout << "You have: "<< gold << " gold"<< endl;
+    cout << "Your ATK: "<< ATK << endl;
+    cout << "Your HP: "<< HP << endl;
     SetConsoleTextAttribute(hConsole, 15); 
     cout << "============================================================================================" <<endl;
 
@@ -50,94 +51,236 @@ int main() {
 
     cout << "============================================================================================" <<endl<<endl;
     cout << "                               4. Back                                                      "<<endl<<endl;
+    cout << "============================================================================================" << endl;
 
-    int choice,count_ATK,count_HP,count_passiveskill;
-    string input, ans;
+    //int choice;//
+    string choice, input;
     while (true) {
-        cout << "Enter your choice (1, 2 or 3): " << endl;
-        getline(cin, input);
-        try {
-            choice = stoi(input);
-            if ((choice == 1) || (choice == 2) || (choice == 3)) {
-                break;
-            }
-            else {
-                cout << "Invalid choice. Please enter 1, 2 or 3." << endl;
-            }
+        cout << "Enter your choice (1, 2, 3 or 4): " << endl;
+        cin >> choice;
+        if ((choice == "1") || (choice == "2") || (choice == "3") || (choice == "4")) 
+        {
+            return choice;
         }
-        catch (const std::invalid_argument& ia) {
-            cout << "Invalid choice. Please enter 1, 2 or 3." << endl;
+        else {
+            cout << "Invalid choice. Please enter 1, 2, 3 or 4." << endl;
         }
+        
+        
     }
+    
 
+}
+
+int main() {
+    int gold = 100;
+    int ATK = 5;
+    int HP = 100;
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); 
+    int choice;
+    int count_ATK=10;
+    int count_HP=10;
+    int count_passiveskill=1;
+    
+    string input, ans,a;
+    while (true) 
+    {
+
+        choice = stoi(showshop(gold,ATK,HP));
+        
+        if (choice == 4)
+        {
+            cout << endl << "you are going back..."<< endl;
+            break;
+        }
     switch (choice) 
     {
         case 1:
+            int pay1;
+            pay1 = abs(count_ATK - 10) * 4 + 20;
             cout <<endl << "============================================================================================" << endl;
-            cout << "Are you ready to uprade your attack damage? (Y/N)" <<endl;
+            cout << "This upgrade will consume you " << pay1 <<" Golds"<<endl;
+            cout << "You have " << count_ATK << " times left" <<endl;
+            cout << "Are you ready to upgrade your attack damage? (Y/N)" <<endl;
             cin >> ans;
             if (ans == "Y" || ans == "Yes" || ans == "yes" || ans == "YES" || ans == "y")
             {
-            if (count_ATK = 0) 
-            {
-                cout << "You're too powerful! You cannot upgrade your attack damage anymore!!"<<endl;
+                if (count_ATK == 0 && gold >= pay1) 
+                {
+                    
+                    cout <<endl << "============================================================================================" << endl;
+                    SetConsoleTextAttribute(hConsole, 12);
+                    cout << "You're too powerful! You cannot upgrade your attack damage anymore!!"<<endl;
+                    SetConsoleTextAttribute(hConsole, 15);
+                    Sleep(3000);
+                    break;
+                }
+                else if (count_ATK <= 10 && count_ATK >= 1 && gold >= pay1) 
+                {
+                    cout <<endl << "============================================================================================" << endl;
+                    gold = gold - pay1;
+                    ATK = ATK +5;
+                    --count_ATK; 
+
+
+                    Sleep(200);
+                    SetConsoleTextAttribute(hConsole, 10);
+                    cout << endl << "You";Sleep(200);cout<<" feel";Sleep(500);cout <<".";Sleep(500);cout <<".";Sleep(500);cout <<". ";
+                    Sleep(200);cout << "something";Sleep(500);cout <<".";Sleep(500);cout <<".";Sleep(500);cout <<". ";
+                    Sleep(200);cout <<"You're";Sleep(200);cout<<" filled with";
+                    Sleep(500);cout <<".";Sleep(500);cout <<".";Sleep(500);cout <<". ";Sleep(100);
+                    SetConsoleTextAttribute(hConsole, 12);
+                    cout<<"POWER." <<endl;
+                    SetConsoleTextAttribute(hConsole, 10);
+                    Sleep(1000);
+                    cout << "Your attack damage has incrased!" << endl;
+                    cout << count_ATK << " times upgrade left!"<<endl;
+                    SetConsoleTextAttribute(hConsole, 15);
+                    Sleep(3000);
+                    
+                    break;
+                }
+                else
+                {
+                Sleep(200);
+                cout<<"Sorry, you don't have enough gold to pay for it..."<<endl;
+                Sleep(5000);
                 break;
-            }
-            else 
-            {
-                cout << endl << "You feel... something...You're filled with POWER." <<endl;
-                cout << "Your attack damage has incrased!" << endl;
-                cout << count_ATK << "times upgrade left!"<<endl;
-                break;
-            }
+                }
+            
             }
             else if (ans == "N" || ans == "No" || ans == "no" || ans == "NO" || ans == "n")
             {
+                
+                break;
+            }
+            else
+            {
+                cout <<endl << "============================================================================================" << endl;
+                cout <<endl<< "Invalid input, Please enter Y or N next time"<<endl;
+                Sleep(2000);
                 break;
             }
 
 
             
         case 2:
+            int pay2;
+            pay2 = abs(count_HP - 10) * 4 + 20;
 
             cout <<endl << "============================================================================================" << endl;
-            cout << "Are you ready to uprade your HP? (Y/N)" <<endl;
+            cout << "This upgrade will consume you " << pay2 <<" Golds"<<endl;
+            cout << "You have " << count_HP << " times left" <<endl;
+            cout << "Are you ready to upgrade your HP? (Y/N)" <<endl;
             cin >> ans;
             if (ans == "Y" || ans == "Yes" || ans == "yes" || ans == "YES" || ans == "y")
             {
-            if (count_HP = 0) 
-            {cout << "You're too powerful! You cannot upgrade your HP anymore!!"<<endl;
-            break;
-            }
-            else 
+            if (count_HP == 0 && gold >= pay2) 
             {
-                cout << endl << "You feel... something...You're filled with STRENGTH."<<endl;
-                cout << "Your maximum HP has increased!" << endl;
-                cout << count_HP << "times upgrade left!"<<endl;
+                cout <<endl << "============================================================================================" << endl;
+                SetConsoleTextAttribute(hConsole, 12);
+                cout << "You're too Strong! You cannot upgrade your HP anymore!!"<<endl;
+                SetConsoleTextAttribute(hConsole, 15);
+                Sleep(3000);
                 break;
             }
+            else if (count_HP <= 10 && count_HP >= 1 && gold >= pay2) 
+            {
+                cout <<endl << "============================================================================================" << endl;
+                gold = gold - pay2;
+                HP = HP+5;
+                --count_HP;
+
+
+                Sleep(200);
+                SetConsoleTextAttribute(hConsole, 10);
+                cout << endl << "You";Sleep(200);cout<<" feel";Sleep(500);cout <<".";Sleep(500);cout <<".";Sleep(500);cout <<". ";
+                Sleep(200);cout << "something";Sleep(500);cout <<".";Sleep(500);cout <<".";Sleep(500);cout <<". ";
+                Sleep(200);cout <<"You're";Sleep(200);cout<<" filled with";
+                Sleep(500);cout <<".";Sleep(500);cout <<".";Sleep(500);cout <<". ";Sleep(100);
+                SetConsoleTextAttribute(hConsole, 14);
+                cout << "STRENGTH."<<endl;
+                SetConsoleTextAttribute(hConsole, 10);
+                Sleep(1000);
+                cout << "Your maximum HP has increased!" << endl;
+                cout << count_HP << " times upgrade left!"<<endl;
+                SetConsoleTextAttribute(hConsole, 15);
+                Sleep(3000);
+                break;
+            }
+            else
+                {
+                Sleep(200);
+                cout<<"Sorry, you don't have enough gold to pay for it..."<<endl;
+                Sleep(5000);
+                break;
+                }
+
             }
             else if (ans == "N" || ans == "No" || ans == "no" || ans == "NO" || ans == "n")
             {
+
                 break;
             }
-
+            else
+            {
+                cout <<endl << "============================================================================================" << endl;
+                cout <<endl<< "Invalid input, Please enter Y or N next time"<<endl;
+                Sleep(2000);
+                break;
+            }
         case 3:
             cout <<endl << "============================================================================================" << endl;
-            cout << "Are you ready to get your Passive Skill? (Y/N)" <<endl;
+            cout << "This upgrade will consume you 500 Golds"<<endl;
+            cout << "Are you ready to receive your Passive Skill? (Y/N)" <<endl;
             cin >> ans;
             if (ans == "Y" || ans == "Yes" || ans == "yes" || ans == "YES" || ans == "y")
             {
+            if (count_passiveskill == 0 && gold >= 500) 
+            {
+                cout <<endl << "============================================================================================" << endl;
+                SetConsoleTextAttribute(hConsole, 12);
+                cout << "You already had it..."<<endl;
+                SetConsoleTextAttribute(hConsole, 15);
+                Sleep(3000);
+                break;
+            }
+            else if (count_passiveskill == 1 && gold >= 500) 
+            {
+                cout <<endl << "============================================================================================" << endl;
+                gold = gold - 500;
+                --count_passiveskill;
+                SetConsoleTextAttribute(hConsole, 10);
+                cout << "You got a passive skill:HP Recover now!"<<endl;
+                SetConsoleTextAttribute(hConsole, 15);
+                Sleep(3000);
+                break;
 
-            if (count_passiveskill = 1){cout << "You already had it..."<<endl;}
-            else if (count_passiveskill = 0) {cout << "You got a passive skill:HP Recover now!"<<endl;}
+            }
+            else
+                {
+                Sleep(200);
+                cout<<"Sorry, you don't have enough gold to pay for it..."<<endl;
+                Sleep(5000);
+                break;
+                }
             }
             else if (ans == "N" || ans == "No" || ans == "no" || ans == "NO" || ans == "n")
             {
                 break;
             }
+            else
+            {
+                cout <<endl << "============================================================================================" << endl;
+                cout <<endl<< "Invalid input, Please enter Y or N next time"<<endl;
+                Sleep(2000);
+                break;
+            }
+    
             
             
+    }
     }
 
     return 0;
