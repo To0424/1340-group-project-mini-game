153d8e46ba616a7b157ae19d77d7bd0d4ee85725
To0424 <127820039+To0424@users.noreply.github.com>
2023-05-03 13:24:09 +0800

    Create shop.cpp

---
+++ shop.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int shop() {
+
+    system("cls");
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); 
+
+    cout << "Welcome to SHOP!" << endl << endl;
+    cout << "============================================================================================" <<endl;
+
+    SetConsoleTextAttribute(hConsole, 12); 
+
+    cout <<"             //>                                                                  " <<endl;
+    cout <<"()          //---------------------------------------------------------(        "<<endl;
+    cout <<"(*)OXOXOXOXO(*>        1.Incrase ATTACK DAMAGE                          \\      "<<endl;
+    cout <<"()          \\\\-----------------------------------------------------------)      "<<endl;
+    cout <<"             \\\\>                                                                  "<<endl;
+    
+    SetConsoleTextAttribute(hConsole, 15); 
+
+    cout << "============================================================================================" <<endl;
+
+    SetConsoleTextAttribute(hConsole, 2); 
+cout << ",d88b.d88b,"<< "                                            "<<"          ,d88b.d88b,          "<<endl;
+cout << "88888888888"<< "                                            "<<"          88888888888           "<<endl;
+cout << "`Y8888888Y'"<< "                  2. Incrase HP             "<<"          `Y8888888Y'             "<<endl;
+cout << "  `Y888Y'  "<< "                                            "<<"            `Y888Y'             "<<endl;
+cout << "    `Y'    "<< "                                            "<<"              `Y'            "<<endl;
+    SetConsoleTextAttribute(hConsole, 15); 
+
+    cout << "============================================================================================" <<endl;
+    cout << "3. Back"<<endl;
+
+    int choice;
+    do {
+        cout << "Enter your choice (1, 2 or 3): ";
+        cin >> choice;
+
+        if (choice != 1 && choice != 2 && choice != 3) {
+            cout << "Invalid choice. Please enter 1, 2 or 3." << endl;
+        }
+    } while (choice != 1 && choice != 2 && choice != 3);
+
+    switch (choice) {
+        case 1:
+            cout << endl << "Starting game..." << endl;
+
+            break;
+        case 2:
+            cout << endl << "Quitting game..." << endl;
+
+            break;
+        case 3:
+            cout << endl << "Going Backing..." << endl;
+            break;
+    }
+
+    return 0;
+}
