d2a29aa2e5121102ffcda681bd76d2006bf07502
vincent chan <vin6012@connect.hku.hk>
2023-05-04 22:25:54 +0800

    bug fix

--- shop.h
+++ shop.h
@@ -2,4 +2,4 @@
 #include <iostream>
 #include <windows.h>
 
-int shop(int& gold, int& ATK, int& HP, int& passive_skill);
+int shop(int& gold, int& ATK, int& HP, int& count_passiveskill, int &count_ATK, int &count_HP);
