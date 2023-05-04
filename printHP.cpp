4f21c8ecbab42de5be3cccf504a54f7c8be4d640
6969Jan6969 <124983095+6969Jan6969@users.noreply.github.com>
2023-05-04 23:12:10 +0800

    Add files via upload

--- printHP.cpp
+++ printHP.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 void print_hp(int &now, int &full){
     double percent=(100*now/full);
-	if (percent==0){cout << "|GAME--OVER|" ;}
-	else if(0 < percent < 10) { cout << "|=         |"; }
-	else if(10 <= percent < 20) { cout << "|==        |"; }
-	else if(20 <= percent < 30) { cout << "|===       |"; }
-	else if(30 <= percent < 40) { cout << "|====      |"; }
-	else if(40 <= percent < 50) { cout << "|=====     |"; }
-	else if(50 <= percent < 60) { cout << "|======    |"; }
-	else if(60 <= percent < 70) { cout << "|=======   |"; }
-	else if(70 <= percent < 80) { cout << "|========  |"; }
-	else if(80 <= percent < 90) { cout << "|========= |"; }
-	else if(90 <= percent <= 100) { cout << "|==========|"; }
+	if (percent==0){cout << "|GAME--OVER|" ;};
+	elif (0<percent && percent<10){cout << "|=         |" ;};
+	elif (10<=percent && percent<20){cout << "|==        |" ;};
+	elif (20<=percent && percent<30){cout << "|===       |" ;};
+	elif (30<=percent && percent<40){cout << "|====      |" ;};
+	elif (40<=percent && percent<50){cout << "|=====     |" ;};
+	elif (50<=percent && percent<60){cout << "|======    |" ;};
+	elif (60<=percent && percent<70){cout << "|=======   |" ;};
+	elif (70<=percent && percent<80){cout << "|========  |" ;};
+	elif (80<=percent && percent<90){cout << "|========= |" ;};
+	elif (90<=percent && percent<=100){cout << "|==========|" };
 }
