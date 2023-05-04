7151ecd2897b80bff46dc54210cae062daa15c18
vincent chan <vin6012@connect.hku.hk>
2023-05-04 22:39:38 +0800

    t

--- printHP.cpp
+++ printHP.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
-#include<printHP.h>
+#include"printHP.h"
 #include<cmath>
 using namespace std;
 void print_hp(int &now, int &full){
     double percent=(100*now/full);
-	if (percent==0){cout << "|GAME--OVER|" ;}
-	elif (0<percent<10){cout << "|=         |" ;}
+	if (percent == 0) { cout << "|GAME--OVER|" };
+	elif(0 < percent < 10) { cout << "|=         |" };
 	elif (10<=percent<20){cout << "|==        |" ;}
 	elif (20<=percent<30){cout << "|===       |" ;}
 	elif (30<=percent<40){cout << "|====      |" ;}
