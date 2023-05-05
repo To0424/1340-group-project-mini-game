7aae20f4a4a126fedcd50b1f99c9ef60003d5074
6969Jan6969 <124983095+6969Jan6969@users.noreply.github.com>
2023-05-05 14:16:03 +0800

    Update print-drawing.cpp

--- print-drawing.cpp
+++ print-drawing.cpp
@@ -9,13 +9,20 @@ void print(int &x) {
     string line;
     //detect which file is printed
     { 
-    if (x==1){filename = "player_being_attack.txt";}
-    else if (x==2){filename="player lose.txt";}
-    else if (x==3){filename="player ready fight.txt";}
-    else if (x==4){filename="player_attack.txt";}
-    else if (x==5){filename="player defense.txt";}
-    else if (x==6){filename="player win.txt";}
-    else if (x==7){filename="player-win-final.txt";}
+    if (x==1)
+        {filename = "player_being_attack.txt";}//open the drawing that player is being attacked
+    else if (x==2)
+        {filename="player lose.txt";}//open the drawing that player lose
+    else if (x==3)
+        {filename="player ready fight.txt";}//open the drawing that the player is going to play the game
+    else if (x==4)
+        {filename="player_attack.txt";}//open the drawing that play is attacked
+    else if (x==5)
+        {filename="player defense.txt";}//open the drawing that player defense the attack
+    else if (x==6)
+        {filename="player win.txt";}//open the drawing that player win one level
+    else if (x==7)
+        {filename="player-win-final.txt";}//open the drawing that player win all level
     }
     ifstream file(filename);//open the file
     //if the file can be opened successfully
